6891c53eff8861b79d61d677a0222be48a069c66
Jo'raqulov O'tkirjon <97774479+FranchiscoMike@users.noreply.github.com>
2022-04-16 13:45:47 +0500

    Initial commit

---
+++ src/test/java/com/example/spring_advanced_real_work/SpringAdvancedRealWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_advanced_real_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedRealWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
